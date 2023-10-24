df6c8fe80b6373eb5cbefe2d90d9e4224a237498
rijantp <rijanriju22@gmail.com>
2023-10-24 15:30:46 +0530

    color changes

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -15,7 +15,7 @@ export class BarChartComponent {
     labels: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
     datasets: [
       { data: [ 65, 59, 80, 81, 56, 55, 40, 89, 67, 45,21,98 ], label: 'Security ratings' ,
-      backgroundColor: 'rgba(150, 56, 212, 0.3)'
+      backgroundColor: '#8c6ce7'
     },
       
     ]
--- src/app/components/doughnut-chart/doughnut-chart.component.ts
+++ src/app/components/doughnut-chart/doughnut-chart.component.ts
@@ -10,11 +10,9 @@ export class DoughnutChartComponent {
   public doughnutChartLabels: string[] = [ '240' ];
   public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
       { data: [ 350, 450, 0 ], label: 'Series A' },
-      // { data: [ 50, 150, 120 ], label: 'Series B' },
-      // { data: [ 250, 130, 70 ], label: 'Series C' }
     ];
 
   public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
-    responsive: false
+    responsive: false,backgroundColor:'#8c6ce7'
   };
 }
